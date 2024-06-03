c5fe89959d74a98f20513966c91f3681f4068995
therealjustinbiele <justin.biele@gmail.com>
2024-06-03 02:11:47 -0700

    chore: update err param type

--- src/middleware/errors.ts
+++ src/middleware/errors.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-export function errorHandler(err: any, req: Request, res: Response) {
+export function errorHandler(err: Error, req: Request, res: Response) {
   console.error(err.stack);
   res.status(500).send({
     message: err.message || 'Internal Server Error',
