7efedf101be5f7f9c85efb50984358ebca1d0732
therealjustinbiele <justin.biele@gmail.com>
2024-06-02 23:25:39 -0700

    feat: define routes and user controller functions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 import express from 'express'
+import {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getUserById,
+  updateUser
+} from './users/user.controller'
 
 const app = express()
+
+app.route('/users')
+  .get(getAllUsers)
+  .post(createUser)
+
+app.route('/users/:id')
+  .get(getUserById)
+  .post(updateUser)
+  .delete(deleteUser)
 
 app.listen(3000, () => console.log('listening on port 3000'))
---
+++ src/users/user.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express'
+
+export async function getAllUsers(req: Request, res: Response) {}
+export async function createUser(req: Request, res: Response) {}
+export async function getUserById(req: Request, res: Response) {}
+export async function updateUser(req: Request, res: Response) {}
+export async function deleteUser(req: Request, res: Response) {}
